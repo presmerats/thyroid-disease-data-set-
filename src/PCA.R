64f525a5bb2cbf0c2bbd446941322909f756141c
Pau Rodriguez <rodriguez.pau@gmail.com>
2018-06-26 19:07:32 +0200

    clustering started

--- src/PCA.R
+++ src/PCA.R
@@ -30,10 +30,78 @@ pca.func2 <- function(data.mice){
 
 pca.func <- function(data,weights){
   library(FactoMineR)
-  my.pca <- PCA(data, quali.sup = c(7,9),ncp=7,row.w = as.vector(weights))
+  par(mfrow=c(1,1))
+  my.pca <- PCA(data, quali.sup = c(22,24),ncp=7,row.w = as.vector(weights))
   summary(data$class)
-  plot(my.pca$ind$coord[,1],my.pca$ind$coord[,2], col=as.numeric(data$class))
+  Dim1 <- my.pca$ind$coord[,1]
+  Dim2 <- my.pca$ind$coord[,2]
+  plot(Dim1,Dim2, col=as.numeric(data$class), main="Individuals factor map (PCA)")
   legend("topright", levels(data$class),text.col =as.numeric(as.factor(levels(data$class))),col =as.numeric(as.factor(levels(data$class))),pch=1 )
   
+
+  
   return(my.pca)
+}
+
+contributions <- function(my.pca){
+  
+  # contribution of each var to the pc's
+  # -> standardized so all have variance 1, all have the same contribution to the total inertia
+  
+  # show the contributions for the first and second dim
+  my.pca$var$contrib[,c(1,2)]
+  
+}
+
+pca.analysis <- function(pca, data.mice, weights){
+  
+  # significant dimensions---------------------
+  pca$eig[,3]
+  # -> 90% inertia 17 princicpal components
+  # plot, elbow rule
+  plot(pca$eig[,1],type="l",
+       xlab="Eigenvectors", ylab="Eigenvalues")
+  
+  
+  # Var contribution-------------------------
+  # to see the contribution of each var to the first 2 pc
+  contribs <- contributions(pca)
+  contribs <- data.frame(contribs)
+  # contribs sorted by first Dim
+  contribs[order(-contribs$Dim.1),]
+  # contribs sorted by second Dim
+  contribs[order(-contribs$Dim.2),]
+  
+  # Var representation------------------------
+  # best represented variables in the first factorial plane
+  n = nrow(pca$var$cos2)
+  # worst_var = sort(rowSums(pca$var$cos2[,1:2], na.rm = FALSE, dims = 1))[1]
+  # worst_var
+  best_vars = sort(rowSums(pca$var$cos2[,1:2], na.rm = FALSE, dims = 1))
+  best_vars_dim1 <- pca$var$cos2[order(-pca$var$cos2[,1]),1:2]
+  best_vars_dim1
+  best_vars_dim2 <- pca$var$cos2[order(-pca$var$cos2[,2]),1:2]
+  best_vars_dim2
+  
+  # target modalities represatives ----------------------
+  # well represented modalities of the target in first fact. plane
+  pca$quali.sup$v.test
+  # we remove the modalities of the referral
+  non.interesting <- c(1,2,3,4,5,6)
+  pca$quali.sup$v.test[-non.interesting,1:2]
+  
+  n = nrow(pca$quali.sup$v.test[-non.interesting,1:2])
+  sorted_modalities = sort(rowSums(pca$quali.sup$v.test[-non.interesting,1:2], na.rm = FALSE, dims = 1))
+  best_sorted_modalities = sort(rowSums(pca$quali.sup$v.test[-non.interesting,1:2], na.rm = FALSE, dims = 1))
+  best_sorted_modalities
+  
+  data.pca <- pca$ind$coord
+  
+  source("PCA_rotation.R")
+  par(mfrow=c(1,1))
+  pca.rt <- pca.rotation(pca, data.mice)
+  varimax(pca$var$cor[,1:7])$loadings
+  
+  
+  return(data.pca)
 }
--- src/PCA_rotation.R
+++ src/PCA_rotation.R
@@ -2,7 +2,6 @@ pca.rotation <- function(pca,data.select){
   
   print(class(pca))
 
-  
   Phi <- pca$var$coord
 
   pc.rot <- varimax(Phi)
@@ -10,25 +9,38 @@ pca.rotation <- function(pca,data.select){
   iden <- rownames(data.select)
   etiq <- names(data.select)
 
-
   Phi.rot <- pc.rot$loadings[1:7,]
-
 
   # Plot the rotated variables
   library(calibrate)
 
   Dim1 <- paste(pca$eig[1,2],"%",sep="")
-  Dim1_X <- paste("Dim1", "(",Dim1,")")
+  Dim1_X <- paste("Dim1", "(",round(Dim1,1),")")
   Dim2 <- paste(pca$eig[2,2],"%",sep="")
-  Dim2_Y <- paste("Dim2", "(",Dim2,")")
+  Dim2_Y <- paste("Dim2", "(",round(Dim1,1),")")
   print(Phi.rot)
   print(pca)
 
   ze <- rep(0,7)
   plot(Phi.rot,xlim=c(-1,1),ylim=c(-1,1),xlab = Dim1_X, ylab = Dim2_Y)
-  text(Phi.rot,labels=etiq, col="blue")
+  #plot(Phi.rot,xlim=c(-0.5,0.5),ylim=c(-0.5,0.5),xlab = Dim1_X, ylab = Dim2_Y)
+  text(Phi.rot,labels=etiq[1:length(Phi.rot)], col="blue")
   arrows(ze,ze,Phi.rot[,1],Phi.rot[,2],length=0.07,col="blue")
   abline(h=0,v=0,col="gray")
   circle(1)
+  
+  # add an excentricity to each label
+  
+  # zoom in 
+  ze <- rep(0,7)
+  #plot(Phi.rot,xlim=c(-1,1),ylim=c(-1,1),xlab = Dim1_X, ylab = Dim2_Y)
+  plot(Phi.rot,xlim=c(-0.33,0.33),ylim=c(-0.33,0.33),xlab = Dim1_X, ylab = Dim2_Y)
+  text(Phi.rot,labels=etiq, col="blue")
+  arrows(ze,ze,Phi.rot[,1],Phi.rot[,2],length=0.07,col="blue")
+  abline(h=0,v=0,col="gray")
+  circle(0.33)
+  
+
+  
   }
 
